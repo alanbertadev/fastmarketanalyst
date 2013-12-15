d53eecfda9decc8607f6495094ac40340867303f
alanbertadev <alanbertadev@gmail.com>
2013-12-15 17:56:56 -0500

    Pylint modifications for OAL Renamed JSON-RPC2 methods to compy with PEP8 standards Updated unit tests and ran

--- app/oal.py
+++ app/oal.py
@@ -4,100 +4,99 @@ from user.account import EmailParameterNotSuppliedException
 from user.account import PasswordParameterNotSuppliedException
 from user.account import PasswordIsIncorrectException
 from user.account import AccountEmailAlreadyExistsException
-import logging
 import base64
 
-class OnlineApplicationLayer:
+class OnlineApplicationLayer(object):
 
-    def createAccount(self,params):
+    def create_account( self, params ):
         """
         Create a new account to monitor a watchlist of securities
-        
+
         B{Example JSON-RPC2 Request:}
-        
-        I{{"jsonrpc": "2.0", "method": "createAccount", "params": {"email":"alan@test.com", "password":"123456"}, "id": 1}}
-        
+
+        I{{"jsonrpc": "2.0", "method": "create_account", "params": {"email":"alan@test.com", "password":"123456"}, "id": 1}}
+
         B{Example JSON-RPC2 Response:}
-        
+
         I{{"jsonrpc": "2.0", "result": { "email": "alan@test.com" }, "id": 1}}
-       
+
         @param params: keys:
-        
+
         E{-} B{email} as I{string}
-        
+
         E{-} B{password} as I{string}
-        
+
         @type params: dictionary
 
         @return:  response keys:
-        
-        E{-} B{email} as I{string} the email that was request to be created. If a valid response (no error code) 
+
+        E{-} B{email} as I{string} the email that was request to be created. If a valid response (no error code)
         """
         if "email" in params:
             if "password" in params:
-                retObj = {"email":""}
+                ret_obj = {"email":""}
                 try:
                     account = Account( email=params["email"], password=params["password"] )
                 except PasswordIsIncorrectException:
                     raise AccountEmailAlreadyExistsException( "Account with email " + params["email"]  + " already exists!" )
                 account.create()
-                retObj["email"] = params["email"]
-                return retObj
+                ret_obj["email"] = params["email"]
+                return ret_obj
             else:
                 raise PasswordParameterNotSuppliedException("Request was missing required \"password\" parameter")
         else:
             raise EmailParameterNotSuppliedException("Request was missing required \"email\" parameter")
-    
-    def getAnnualIncomeStatementAsHtml(self, params):
+
+    def get_annual_income_statement_as_html( self, params ):
         """
         Fetch(from Yahoo.com) the annual income statement of a given security
-        
+
         B{Example JSON-RPC2 Request:}
-        
-        I{{"jsonrpc": "2.0", "method": "getAnnualIncomeStatementAsHtml", "params": {"symbol":"msft"}, "id": 1}}
-        
+
+        I{{"jsonrpc": "2.0", "method": "get_annual_income_statement_as_html", "params": {"symbol":"msft"}, "id": 1}}
+
         B{Example JSON-RPC2 Response:}
-        
-        I{{"jsonrpc": "2.0", "result": { "IncomeStatement": "<base 64 encoded HTML>" }, "id": 1}}
-       
+
+        I{{"jsonrpc": "2.0", "result": { "income_statement": "<base 64 encoded HTML>" }, "id": 1}}
+
         @param params: keys:
-        
+
         E{-} B{symbol} as I{string}
-        
+
         @type params: dictionary
 
         @return:  response keys:
-        
-        E{-} B{IncomeStatement} as I{string} a Base64 encoded HTML of the annual income statement
-        """
-        retObj = {'IncomeStatement':""}
-        incomeStatementMgr = IncomeStatementManager(params['symbol'])
-        retObj['IncomeStatement'] = base64.b64encode(incomeStatementMgr.getAnnual().getHtml())
-        return retObj
 
-    def getQuarterlyIncomeStatementAsHtml(self, params):
+        E{-} B{income_statement} as I{string} a Base64 encoded HTML of the annual income statement
+        """
+        ret_obj = {'income_statement':""}
+        income_statement_manager = IncomeStatementManager(params['symbol'])
+        ret_obj['income_statement'] = base64.b64encode(income_statement_manager.getAnnual().getHtml())
+        return ret_obj
+
+    def get_quarterly_income_statement_as_html( self, params ):
         """
         Fetch(from Yahoo.com) the quarterly income statement of a given security
-        
+
         B{Example JSON-RPC2 Request:}
-        
-        I{{"jsonrpc": "2.0", "method": "getQuarterlyIncomeStatementAsHtml", "params": {"symbol":"goog"}, "id": 1}}
+
+        I{{"jsonrpc": "2.0", "method": "get_quarterly_income_statement_as_html", "params": {"symbol":"goog"}, "id": 1}}
 
         B{Example JSON-RPC2 Response:}
-        
-        I{{"jsonrpc": "2.0", "result": { "IncomeStatement": "<base 64 encoded HTML>" }, "id": 1}}
-        
+
+        I{{"jsonrpc": "2.0", "result": { "income_statement": "<base 64 encoded HTML>" }, "id": 1}}
+
         @param params: keys:
-        
+
         E{-} B{symbol} as I{string}
-        
+
         @type params: dictionary
 
         @return:  response keys:
-        
-        E{-} B{IncomeStatement} as I{string} a Base64 encoded HTML of the quarterly income statement
+
+        E{-} B{income_statement} as I{string} a Base64 encoded HTML of the quarterly income statement
         """
-        retObj = {'IncomeStatement':""}
-        incomeStatementMgr = IncomeStatementManager(params['symbol'])
-        retObj['IncomeStatement'] = base64.b64encode(incomeStatementMgr.getQuarterly().getHtml())
-        return retObj
+        ret_obj = {'income_statement':""}
+        income_statement_manager = IncomeStatementManager(params['symbol'])
+        ret_obj['income_statement'] = base64.b64encode(income_statement_manager.getQuarterly().getHtml())
+        return ret_obj
--- tests/test_mod_python_endpoint.py
+++ tests/test_mod_python_endpoint.py
@@ -10,7 +10,7 @@ class TestJson2FmaHandler(unittest.TestCase):
         redis = RedisFacade()
     
         json2FmaHandler = Json2FmaHandler()
-        testString =  "{\"jsonrpc\": \"2.0\", \"method\": \"createAccount\", \"params\": {\"email\": \"alanbertatesting@test.com\", \"password\":\"123456\"}, \"id\": 0}"
+        testString =  "{\"jsonrpc\": \"2.0\", \"method\": \"create_account\", \"params\": {\"email\": \"alanbertatesting@test.com\", \"password\":\"123456\"}, \"id\": 0}"
         
         resData = json2FmaHandler.handle( testString )
         
@@ -23,14 +23,14 @@ class TestJson2FmaHandler(unittest.TestCase):
     def test_fetch_google_annual_income_statement_as_html(self):
     
         json2FmaHandler = Json2FmaHandler()
-        testString =  "{\"jsonrpc\": \"2.0\", \"method\": \"getAnnualIncomeStatementAsHtml\", \"params\": {\"symbol\": \"GOOG\"}, \"id\": 0}"
+        testString =  "{\"jsonrpc\": \"2.0\", \"method\": \"get_annual_income_statement_as_html\", \"params\": {\"symbol\": \"GOOG\"}, \"id\": 0}"
     
         assert json.loads(json2FmaHandler.handle( testString ))['result'] is not None
         
     def test_fetch_google_quarterly_income_statement_as_html(self):
     
         json2FmaHandler = Json2FmaHandler()
-        testString =  "{\"jsonrpc\": \"2.0\", \"method\": \"getQuarterlyIncomeStatementAsHtml\", \"params\": {\"symbol\": \"GOOG\"}, \"id\": 0}"
+        testString =  "{\"jsonrpc\": \"2.0\", \"method\": \"get_quarterly_income_statement_as_html\", \"params\": {\"symbol\": \"GOOG\"}, \"id\": 0}"
     
         assert json.loads(json2FmaHandler.handle( testString ))['result'] is not None
         
